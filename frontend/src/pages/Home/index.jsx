8021893636c6b364d792d4de92c8ebc1560587c9
Quentin Rollet <rollet.qunetin@orange.fr>
2023-02-28 22:07:02 +0100

    useFetch + loader + Porfolio Info
    Creation of the useFetch hook to get some data from the API, create a new loader and start to do api calls for Portfolio Page

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,8 +1,93 @@
+import styled from "styled-components";
+import MyPhoto from "../../asset/photo_quentin.JPG";
+import DatacenterPhoto from "../../asset/datacenter_server.jpg";
+import GraphicalTabletPhoto from "../../asset/tablette_graphique.jpg";
+import { useContext } from "react";
+import { ThemeContext } from "../../utils/context";
+import { Link } from "react-router-dom";
+
+const HomeContainer = styled.div`
+    background-color: ${(props) => props.thememode.backgroundLight};
+    border-radius: 10px;
+    margin: 50px;
+
+    & > a {
+        text-decoration: none;
+        color: ${(props) => props.thememode.text};
+        display: flex;
+        text-align: justify;
+        margin: 50px;
+        padding: 50px;
+    }
+
+    & > a > div {
+        padding: 50px;
+    }
+`;
+
+const ImageContainer = styled.img`
+    width: 300px;
+    height: 300px;
+    border: black 3px solid;
+    border-radius: 50%;
+    flex-shrink: 0;
+`;
+
 function Home() {
+    const { themeMode } = useContext(ThemeContext);
     return (
-        <div>
-            <h1>Welcome Home !</h1>
-        </div>
+        <HomeContainer thememode={themeMode}>
+            <Link to="aboutMe">
+                <ImageContainer src={MyPhoto} />
+                <div>
+                    <h1>À propos de moi</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Placeat, dignissimos nemo. Veniam porro soluta eligendi,
+                        dolor qui pariatur culpa enim nam illum asperiores
+                        commodi, ut totam alias rem itaque possimus! Lorem ipsum
+                        dolor sit, amet consectetur adipisicing elit. Cum
+                        tenetur assumenda incidunt alias repudiandae unde nam
+                        odit harum obcaecati autem voluptatum laboriosam illo,
+                        error architecto maiores porro id hic placeat?
+                    </p>
+                </div>
+            </Link>
+            <Link to="portfolioInfo">
+                <div>
+                    <h1>Mes projets informatique</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Placeat, dignissimos nemo. Veniam porro soluta eligendi,
+                        dolor qui pariatur culpa enim nam illum asperiores
+                        commodi, ut totam alias rem itaque possimus! Lorem ipsum
+                        dolor sit, amet consectetur adipisicing elit. Cum
+                        tenetur assumenda incidunt alias repudiandae unde nam
+                        odit harum obcaecati autem voluptatum laboriosam illo,
+                        error architecto maiores porro id hic placeat?
+                    </p>
+                </div>
+                <ImageContainer src={DatacenterPhoto} />
+            </Link>
+            <Link></Link>
+            <Link to="portfolioArt">
+                <ImageContainer src={GraphicalTabletPhoto} />
+                <div>
+                    <h1>Mes oeuvres d'arts</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Placeat, dignissimos nemo. Veniam porro soluta eligendi,
+                        dolor qui pariatur culpa enim nam illum asperiores
+                        commodi, ut totam alias rem itaque possimus! Lorem ipsum
+                        dolor sit, amet consectetur adipisicing elit. Cum
+                        tenetur assumenda incidunt alias repudiandae unde nam
+                        odit harum obcaecati autem voluptatum laboriosam illo,
+                        error architecto maiores porro id hic placeat?
+                    </p>
+                </div>
+            </Link>
+            <Link></Link>
+        </HomeContainer>
     );
 }
 
--- frontend/src/pages/PortfolioInfo/index.jsx
+++ frontend/src/pages/PortfolioInfo/index.jsx
@@ -1,7 +1,30 @@
+import { useFetch } from "../../utils/hooks";
+import { Loader } from "../../utils/Atoms";
+
 function PortfolioInfo() {
+    const { data, isLoading, error } = useFetch(
+        "http://localhost:8000/api/projectsInfo"
+    );
+    const { projects } = data;
+
+    if (error) {
+        return <div>Oups, il y a un problème</div>;
+    }
+
     return (
         <div>
-            <h1>PortfolioInfo</h1>
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <div>
+                    {projects.map((project) => (
+                        <div key={`${project._id}`}>
+                            <h1>{project.title}</h1>
+                            <p>{project.description}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
